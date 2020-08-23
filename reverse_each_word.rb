fc9317dd1e370b20a231311f70d94b9dbd1c158d
Nabeil Lemar <nabeillemar@gmail.com>
2020-08-23 14:51:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word(array)
   new_array = array.split(" ")
   reverse_array = new_array.each {|x| x.reverse}
-  return reverse_array.join
+  return reverse_array.join(" ")
+end
   
